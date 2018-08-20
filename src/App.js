9f48fad18c2fd38c9af0e22871ab28a897f90d90
Rastko Sasic <rsasic@europe.com>
2018-08-19 21:57:38 -0400

    react-create-app on Heroku

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Clock from './components/Clock'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        </Clock>
+      </div>
+    );
+  }
+}
+
+export default App;
