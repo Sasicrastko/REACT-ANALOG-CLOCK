7fe9172ddfd214fb008e44887517794045548df3
Rastko Sasic <rsasic@europe.com>
2018-08-19 23:17:43 -0400

    hands are visible

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -8,6 +8,16 @@ class Clock extends Component {
             <div className="container">
                 <div className="outer-circle">
                     <div className="inner-circle">
+                        <div className="minute-hand">
+                        <div className="visible-part-min-hand"></div>
+                        </div>
+                        <div className="hour-hand">
+                        <div className="visible-part-hour-hand"></div>
+                        </div>
+                        <div className="second-hand">
+                        <div className="visible-part-sec-hand"></div>
+                        </div>
+                        <div className="center-dot"></div>
                     </div>
                 </div>
           </div>
