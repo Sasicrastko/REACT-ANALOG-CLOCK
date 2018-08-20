ac1ce3ff694925d6392b7bd35be38c84361233f9
Rastko Sasic <rsasic@europe.com>
2018-08-20 00:04:08 -0400

    moving hands by using Date and setInterval

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -3,9 +3,24 @@ import './Clock.css';
 
 class Clock extends Component {
     state = {
+        timer: setInterval(this.setTime.bind(this), 1000),
         hourHandAngle: 90,
         minuteHandAngle: 30,
         secondHandAngle: 180
+    }
+
+    setTime() {
+        const d = new Date();
+        const sec = d.getSeconds();
+        const min = d.getMinutes();
+        const hour = d.getHours();
+        console.log(`${hour}:${min}:${sec}`);
+
+        this.setState({
+        	secondHandAngle:  (sec*360)/60-90, //-90 because CSS set hands horizontaly when angle is 0deg,
+        	minuteHandAngle: (min*360)/60-90,
+       	    hourHandAngle: (hour*360)/12+(min*30)/60-90
+        });
     }
 
     render() { 
