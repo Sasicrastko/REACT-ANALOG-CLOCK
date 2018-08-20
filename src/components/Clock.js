1a634538f8d21769580219d99725ed4d2da9be89
Rastko Sasic <rsasic@europe.com>
2018-08-19 22:41:20 -0400

    add some style

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,22 +2,12 @@ import React, { Component } from 'react';
 import './Clock.css';
 
 class Clock extends Component {
-    state = {  }
+    state = {}
     render() { 
         return (
             <div className="container">
                 <div className="outer-circle">
                     <div className="inner-circle">
-                        <div className="minute-hand">
-                        <div className="visible-min"></div>
-                        </div>
-                        <div className="hour-hand">
-                        <div className="visible-hour"></div>
-                        </div>
-                        <div className="second-hand">
-                        <div className="visible-sec"></div>
-                        </div>
-                        <div className="center-dot"></div>
                     </div>
                 </div>
           </div>
