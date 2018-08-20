6ff265709ba9a8a4bf0b1911aa3f9e1455453f9e
Rastko Sasic <rsasic@europe.com>
2018-08-19 23:37:05 -0400

    setting hands to different angle positions

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,19 +2,24 @@ import React, { Component } from 'react';
 import './Clock.css';
 
 class Clock extends Component {
-    state = {}
+    state = {
+        hourHandAngle: 90,
+        minuteHandAngle: 30,
+        secondHandAngle: 180
+    }
+
     render() { 
         return (
             <div className="container">
                 <div className="outer-circle">
                     <div className="inner-circle">
-                        <div className="minute-hand">
+                        <div className="minute-hand" style={{transform : `rotate(${this.state.minuteHandAngle}deg)`}}>
                         <div className="visible-part-min-hand"></div>
                         </div>
-                        <div className="hour-hand">
+                        <div className="hour-hand" style={{transform : `rotate(${this.state.hourHandAngle}deg)`}}>
                         <div className="visible-part-hour-hand"></div>
                         </div>
-                        <div className="second-hand">
+                        <div className="second-hand" style={{transform : `rotate(${this.state.secondHandAngle}deg)`}}>
                         <div className="visible-part-sec-hand"></div>
                         </div>
                         <div className="center-dot"></div>
